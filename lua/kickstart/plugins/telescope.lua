2407e32c29ea668ccf001998ceb7e8b2f422df6c
Pratish <pratish@birdcave.net>
2025-04-16 09:30:31 +0200

    Set nerd font correctly this time

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -28,8 +28,7 @@ return {
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       -- Not sure why this is not working with Hack font, but for now enabled = true works fine
-      --{ 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-      { 'nvim-tree/nvim-web-devicons', enabled = true },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
