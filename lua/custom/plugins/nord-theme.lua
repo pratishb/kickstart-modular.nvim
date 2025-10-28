f373f102490aa1b8d8cf659454885f20d6765a58
Pratish <pratish@birdcave.net>
2025-10-28 11:30:33 +0200

    Added themes

--- lua/custom/plugins/nord-theme.lua
+++ lua/custom/plugins/nord-theme.lua
@@ -4,18 +4,16 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'shaunsingh/nord.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    lazy = false,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('catppuccin').setup {
-        flavour = 'macchiato', -- latte, frappe, macchiato, mocha
-      }
+      -- require('nord').set {}
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'catppuccin'
+      -- vim.cmd.colorscheme 'nord'
     end,
   },
 }
